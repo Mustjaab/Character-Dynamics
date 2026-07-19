f8d10bef1bf260af7e27525c41abc65027801399
Mustjaab <95559087+Mustjaab@users.noreply.github.com>
2026-07-19 14:05:16 -0400

    Update character-network.js

--- character-network.js
+++ character-network.js
@@ -110,9 +110,9 @@ function myCreateFunction() {
   var cell1 = row.insertCell(0);
   var cell2 = row.insertCell(1);
   var cell3 = row.insertCell(2);
-  cell1.innerHTML = "NEW CELL1";
-  cell2.innerHTML = "NEW CELL2";
-  cell3.innerHTML = "NEW CELL2"; 
+  cell1.innerHTML = "<input id='source6'></input>";
+  cell2.innerHTML = "<input id='interaction6'></input>";
+  cell3.innerHTML = "<input id='target6'></input>"; 
 }
 
 function myDeleteFunction() {
